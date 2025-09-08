bb2786fcc6bbd6acbdcf2d6c985c9ed028593c36
Diane NANA <diane.nana@mboadigital.tech>
2025-09-08 12:20:21 +0100

    handle POO

---
+++ learcsharp/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using learcsharp;
+
+Console.WriteLine("Hello, World!");
+
+Car car = new Car("Toyota", "Lexus", true);
+Customer customer = new Customer("Diane", "123 Main Street", "diane@gmail.com");
+customer.SetDetail("I am a customer");
+
+car.Drive();
+
+Console.WriteLine(car.Brand);
