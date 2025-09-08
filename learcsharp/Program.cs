f9d64e2098407145ff5551c2c18c64b7bae95e5a
Diane NANA <devdiane084@gmail.com>
2025-09-08 12:34:04 +0100

    handle 3 type of remove

--- learcsharp/Program.cs
+++ learcsharp/Program.cs
@@ -1,12 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using learcsharp;
 
-Console.WriteLine("Hello, World!");
+//creating a new list and initialise it 
 
-Car car = new Car("Toyota", "Lexus", true);
-Customer customer = new Customer("Diane", "123 Main Street", "diane@gmail.com");
-customer.SetDetail("I am a customer");
+List<string> fruits = new List<string> { "Apple", "Banana", "Mango", "Orange" };
 
-car.Drive();
+Console.WriteLine("Original List:");
+foreach (var fruit in fruits)
+{
+    Console.WriteLine(fruit);
+}
 
-Console.WriteLine(car.Brand);
+// 1. Remove by value
+fruits.Remove("Banana");  // Removes the first occurrence of "Banana"
+
+// 2. Remove by index
+fruits.RemoveAt(1);       // Removes the item at index 1 (here "Mango")
+
+// 3. Remove all items that match a condition (using lambda)
+fruits.RemoveAll(f => f.StartsWith("O")); // Removes "Orange"
+
+// 4. Clear the whole list
+// fruits.Clear();
+
+Console.WriteLine("\nList after removals:");
+foreach (var fruit in fruits)
+{
+    Console.WriteLine(fruit);
+}
