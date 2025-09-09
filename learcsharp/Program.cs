dc8a5ed5cb52fb70ff68b7417e7b7e6a355eb269
Diane NANA <devdiane084@gmail.com>
2025-09-09 14:49:50 +0100

    handle private protected and public

--- learcsharp/Program.cs
+++ learcsharp/Program.cs
@@ -1,48 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using learcsharp;
 
-// Héritage simple
-Chien rex = new Chien { Nom = "Rex" };
-rex.SeDeplacer();   // de Animal
-rex.Allaiter();     // de Mammifere
-rex.Aboyer();       // de Chien
+Chien rex = new Chien();
+rex.Nom = "Rex";           // OK : public
+rex.AfficherInfo();        // OK : méthode publique
+rex.Presenter();           // OK
 
-// Héritage hiérarchique
-Chat minou = new Chat { Nom = "Minou" };
-minou.SeDeplacer();
-minou.Miauler();
-
-// Polymorphisme
-Animal a = new Chien { Nom = "Poli" };
-a.SeDeplacer(); // méthode d'Animal
-
-// Interface
-IDomestique domestique = new Chien();
-domestique.VivreAvecHumain();
 class Animal
 {
-    public string Nom { get; set; }
-    public void SeDeplacer() => Console.WriteLine("Je me déplace.");
+    private string secret = "Je suis privé";      // uniquement dans Animal
+    protected string espece = "Animal";           // accessible dans Animal + dérivées
+    public string Nom { get; set; }               // accessible partout
+
+    public void AfficherInfo()
+    {
+        Console.WriteLine($"Nom: {Nom}, Espèce: {espece}");
+    }
 }
 
-class Mammifere : Animal
+class Chien : Animal
 {
-    public void Allaiter() => Console.WriteLine("Je nourris mes petits.");
-}
-
-interface IDomestique
-{
-    void VivreAvecHumain();
-}
-
-class Chien : Mammifere, IDomestique
-{
-    public void Aboyer() => Console.WriteLine("Wouf !");
-    public void VivreAvecHumain() => Console.WriteLine("Je vis avec les humains comme animal de compagnie.");
-}
-
-class Chat : Mammifere, IDomestique
-{
-    public void Miauler() => Console.WriteLine("Miaou !");
-    public void VivreAvecHumain() => Console.WriteLine("Je vis avec les humains, mais je suis indépendant !");
+    public void Presenter()
+    {
+        // Console.WriteLine(secret); ❌ Erreur : pas accessible (private)
+        Console.WriteLine($"Je suis un {espece} et je m'appelle {Nom}"); // OK (protected + public)
+    }
 }
